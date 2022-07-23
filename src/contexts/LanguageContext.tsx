d365102bcca30c24578c9004c416e3c98313f896
cassio-silva <cassioglopesdasilva@hotmail.com>
2022-07-23 02:41:00 -0300

    Small css fixes on page /pokecoins

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -16,6 +16,7 @@ type LanguageContextProps = {
     hours: string;
     minutes: string;
     buttonMessage: string;
+    coinsPerDay: string;
   };
 }
 
--- src/pages/pokecoins.tsx
+++ src/pages/pokecoins.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { NextPage } from "next";
 import { Navbar } from "../components/Navbar";
 import styled, { css } from "styled-components";
@@ -10,15 +10,18 @@ const Pokecoins: NextPage = () => {
   const [hours, setHours] = useState("");
   const [minutes, setMinutes] = useState("");
   const [isShown, setIsShown] = useState(false);
+  const [alert, setAlert] = useState(false);
   const [coins, setCoins] = useState<number>(0);
   const { langContent } = useContext(LanguageContext);
+  const coinRef = useRef<any>();
 
   useEffect(() => {
-    if (Number(minutes) > 59) {
-      setMinutes("0");
+    if (Number(minutes) >= 60) {
+      setMinutes("1");
       setHours((Number(hours) + 1).toString())
-    } else if (Number(minutes) < 1) {
-      setMinutes("");
+    } else if (Number(minutes) < 0) {
+      setMinutes("59");
+      setHours((Number(hours) - 1).toString())
     }
   }, [minutes])
 
@@ -37,9 +40,16 @@ const Pokecoins: NextPage = () => {
     const totalCoins = (h * 6) + Math.floor(min * 0.1);
 
     if (totalCoins > 50) {
-      return setCoins(50)
+      setCoins(50);
+      if (!alert) {
+        setAlert(true);
+        setTimeout(() => {
+          setAlert(false)
+        }, 6000);
+      }
+      coinRef.current.focus();
     } else {
-      return setCoins(totalCoins)
+      setCoins(totalCoins);
     }
   }
 
@@ -79,15 +89,27 @@ const Pokecoins: NextPage = () => {
             placeholder={langContent.minutes}
           />
         </GymTimeForm>
+        <span className={alert ? "alert" : ""}>{langContent.coinsPerDay}</span>
         <p>
           {langContent.buttonMessage.split("pokecoin")[0]}<br />
           {langContent.buttonMessage.split("pokecoin")[1]}
         </p>
-        <Separator isShown={isShown}>
-          <button onClick={handleCoinCalculation}>
-            <img src="/assets/pokecoin.png" alt="Pokecoin" />
+        <Separator isShown={isShown} coins={coins}>
+          <button
+            key={`btn${coins}`}
+            onClick={handleCoinCalculation}
+          >
+            <img src="/assets/pokecoin.png" alt="Pokecoin" draggable="false" />
           </button>
-          <span key={coins}>{coins}</span>
+          <span
+            id="coins"
+            key={coins}
+            ref={coinRef}
+            tabIndex={0}
+            aria-label={`moedas: ${coins}, coins: ${coins}`}
+          >
+            {coins}
+          </span>
         </Separator>
       </Content>
     </GlobalContainer>
@@ -96,7 +118,7 @@ const Pokecoins: NextPage = () => {
 
 export default Pokecoins;
 
-const Content = styled.section`
+const Content = styled.section<any>`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -106,20 +128,36 @@ const Content = styled.section`
   width: 95%;
   max-width: 40rem;
   margin-top: 2rem;
-
-  img {
-    width: 100%;
-  }
-
+  
   h2 {
     font-size: 1.75rem;
     font-weight: 600;
     color: #FFF;
     text-align: center;
   }
+  span {
+    height: 0;
+    font-weight: bold;
+    overflow: hidden;
+    transition: 0.2s;
+    color: #ffbf00;
+    text-align: center;
+    
+    &.alert {
+      height: 55px;
+    }
+  }
   p {
     color: ${props => props.theme.color.gray[50]};
     text-align: center;
+  }
+
+  @media (min-width: 768px) {
+    span {
+      &.alert {
+      height: 30px;
+    }
+    }
   }
 `;
 
@@ -129,6 +167,7 @@ const GymTimeForm = styled.form`
   align-items: center;
 
   margin: 1rem auto;
+  position: relative;
 
   input {
     width: 7rem;
@@ -170,47 +209,66 @@ const GymTimeForm = styled.form`
 
 const Separator = styled.div<any>`
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-between;
   align-items: center;
   position: relative;
   margin: 2rem auto;
-
-  &:first-child {
-    width: 50%;
-  }
-  &:last-child {
-    width: 50%;
-  }
+  width: 14rem;
 
   button {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    
     border: none;
     border-radius: 50%;
     width: 7rem;
+    height: 7rem;
     background: transparent;
     position: relative;
 
     transition: 0.2s;
+    box-sizing: border-box;
+
+    animation: 0.5s btnAnimation ease-in-out;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
 
     &:hover {
-      filter: brightness(1.3);
+      filter: brightness(1.2);
       transform: rotate3d(360deg);
+    }
+
+    @keyframes btnAnimation {
+      from {
+        transform: scale(95%);
+      }
+      to {
+        transform: scale(100%);
+      }
     }
   }
 
-  span {
-    font-size: 3rem;
+  & > span {
+    font-size: 4rem;
+    line-height: 90%;
     font-weight: 800;
     color: #FFF;
+    width: 5.5rem;
+    height: fit-content;
+    animation: ${props => props.isShown && "1s fadeIn ease-in-out"};
+    transition: 0.2s;
+    text-align: unset;
 
-    ${props => props.isShown && css`
-      animation: 1s fadeIn ease-in-out;
+    ${props => props.coins && css`
+      transform: translateX(1%);
     `}
-
-    @keyframes fadeIn {
-      from {
-        opacity: 0;        
-      }
-      to {opacity: 1}
-    }
+  }
+  @keyframes fadeIn {
+    0% {opacity: 0}
+    100% {opacity: 1}
   }
 `;
