3e5d5ac02b2c96fedb49835e7257005687c548af
cassio-silva <cassioglopesdasilva@hotmail.com>
2023-11-15 20:48:45 -0300

    fix: Scroll hidden on first load

--- src/components/global/BackToTopButton/index.tsx
+++ src/components/global/BackToTopButton/index.tsx
@@ -5,13 +5,17 @@ import { ChevronDoubleUpIcon } from '@heroicons/react/24/solid'
 
 export function ButtonBackToTop() {
   const [isVisible, setIsVisible] = useState(false)
-
+  
   function handleScroll() {
     const scrollPosition = window.scrollY
     const windowHeight = window.innerHeight
     const documentHeight = document.body.scrollHeight
 
-    if (scrollPosition > documentHeight / 2 - windowHeight / 2) {
+    if (documentHeight / 3 - windowHeight / 3 <= 0) {
+      return
+    }
+
+    if (scrollPosition > documentHeight / 3 - windowHeight / 3) {
       setIsVisible(true)
     } else {
       setIsVisible(false)
@@ -35,8 +39,8 @@ export function ButtonBackToTop() {
 
   return (
     <button
-      data-hidden={!isVisible}
-      className={`fixed data-[hidden=true]:hidden bottom-20 lg:bottom-8 right-4 border-2 rounded-lg border-white 
+      data-visible={isVisible}
+      className={`hidden fixed data-[visible=true]:block animate-fadeIn bottom-20 lg:bottom-6 right-4 border-2 rounded-lg border-white 
       text-white bg-gradient-to-br from-yellow-300 to-orange-300 `}
       onClick={handleBackToTop}
     >
